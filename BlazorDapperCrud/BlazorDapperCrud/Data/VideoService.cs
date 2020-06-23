5e78518784f2eee523e48c566bbd41998c815979
jaco <jaco.kleynhans@icloud.com>
2020-06-23 14:30:24 +0200

    First Commit

---
+++ BlazorDapperCrud/BlazorDapperCrud/Data/VideoService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BlazorDapperCrud.Data
+{
+    public class VideoService : IVideoService
+    {
+        //database connections
+        private readonly SqlConnectionConfiguration _configuration;
+
+        public VideoService(SqlConnectionConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //add record
+        public async Task<bool> VideoInsert(Video video)
+        {
+            using (var conn = new SqlConnection(_configuration.Value))
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("Title", video.Title, DbType.String);
+                parameters.Add("DatePublished", video.DatePublished, DbType.Date);
+                parameters.Add("IsActive", video.IsActive, DbType.Boolean);
+                const string query =
+                    "INSERT INTO Video(Title,DatePublished,IsActive)VALUES(@Title, @DatePublished, @IsActive)";
+                await conn.ExecuteAsync(query, new {video.Title, video.DatePublished, video.IsActive},
+                    commandType: CommandType.Text);
+            }
+
+            return true;
+        }
+    }
+}
